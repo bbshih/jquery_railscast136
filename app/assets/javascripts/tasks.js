7e129823bf76501283556c3099b27cba6c3bbf0b
Billy Shih <me@billyshih.com>
2013-06-12 14:42:42 -0700

    Completed exercise

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.edit_task').submitOnCheck();
+});
+
+jQuery.fn.submitOnCheck = function () {
+  this.find('input[type=submit]').remove();
+  this.find('input[type=checkbox]').click(function () {
+    $(this).parent('form').submit();
+  });
+  return this;
+};
